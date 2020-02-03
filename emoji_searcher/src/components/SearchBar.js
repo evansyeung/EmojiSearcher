79b810994e13cc39c2778f610570b4c295114f59
Evans Yeung <evansbyeung@gmail.com>
2020-02-03 10:49:35 -0500

    Change from filter on submit to input text change

--- emoji_searcher/src/components/SearchBar.js
+++ emoji_searcher/src/components/SearchBar.js
@@ -3,19 +3,15 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { searchTerm: "" };
 
-  onFormSubmit = e => {
-    e.preventDefault();
-    this.props.onSearchTermSubmit(this.state.searchTerm);
-  };
-
   onInputSearchChange = e => {
     this.setState({ searchTerm: e.target.value });
+    this.props.onSearchTermSubmit(e.target.value);
   };
 
   render() {
     return (
       <div className="search-bar ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
+        <form onSubmit={e => e.preventDefault()} className="ui form">
           <div className="field">
             <label>Emoji Search</label>
             <input
